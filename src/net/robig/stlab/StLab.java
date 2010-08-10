1bee92401ef3de5909876b88fb78d5714861c213
robig <robig@3e5e3696-bdf4-0310-96c2-9c50ceb56a63>
2010-08-10 14:52:13 +0000

    Error message when midi device not available

--- src/net/robig/stlab/StLab.java
+++ src/net/robig/stlab/StLab.java
@@ -1,5 +1,6 @@
 package net.robig.stlab;
 
+import javax.swing.JOptionPane;
 import net.robig.stlab.gui.DeviceFrame;
 import net.robig.stlab.midi.DeviceController;
 import net.robig.stlab.midi.DeviceNotFoundException;
@@ -7,11 +8,16 @@ import net.robig.stlab.midi.AbstractMidiController;
 import net.robig.stlab.midi.MidiControllerFactory;
 
 public class StLab {
-    public static void main(String[] args) throws DeviceNotFoundException {
+    public static void main(String[] args) throws Exception {
 
     	//TODO: show loading screen
-		final AbstractMidiController midiController=MidiControllerFactory.create();
-		midiController.findAndConnectToVOX();
+    	try {
+			final AbstractMidiController midiController=MidiControllerFactory.create();
+			midiController.findAndConnectToVOX();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error initializing Midi system: "+e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			throw e;
+		}
 		
 		// main window:
 		DeviceFrame deviceFrame = new DeviceFrame(new DeviceController());
