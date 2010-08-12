4b9ef609fbee145e3fc423162e4b6734d3240c45
robig <robig@3e5e3696-bdf4-0310-96c2-9c50ceb56a63>
2010-08-12 13:02:28 +0000

    binary testing

--- readme.txt
+++ readme.txt
@@ -1,6 +1,19 @@
-TODO:
-- Poll for device availability!
+            StLab - graphical interface for the Tonelab ST
+            =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+            
+TODO Version 0.1:
+- Model: decode & encode full program data (start with on/off switches)
+- GUI: shift/alt activates option mode
+- GUI: use config for sensitivity
+- GUI: save button
+- GUI: 3-way LED for amp type
+- GUI: amp type button functionality
+- Incoming commands (preset change, save preset)
+- Poll for device availability, program change!
 
-SŠter:
-- preset uplaod to server
+Planned for Version 1.0:
+- Preset list (request from and save to device) 
+
+LATER/Feature requests:
+- preset upload to server
 - print preset list
--- test/net/robig/stlab/midi/ModelTest.java
+++ test/net/robig/stlab/midi/ModelTest.java
@@ -1,8 +1,41 @@
 package net.robig.stlab.midi;
 
+import java.util.BitSet;
+
+import net.robig.logging.Logger;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 @Test
 public class ModelTest {
+	Logger log = new Logger(this.getClass());
+	
+	private static int feat1=1;
+	private static int feat2=4;
+	private static int feat3=16;
+	
+	private boolean compare(int value, int feature){
+		return (value&feature) > 0;
+	}
+	
 	//TODO!
+	public void test() {
+		int value=5;
+		//BitSet bits = new BitSet(value);
+		//log.debug(5&3?"5&3: ja":"nein");
+		//5&8
+		assertTrue(compare(value,feat1));
+		assertTrue(compare(value,feat2));
+		assertTrue(compare(value,feat3)==false);
+		
+		value=32;
+		assertTrue(compare(value,feat1)==false);
+		assertTrue(compare(value,feat2)==false);
+		assertTrue(compare(value,feat3)==false);
+		
+		value=17;
+		assertTrue(compare(value,feat1)==true);
+		assertTrue(compare(value,feat2)==false);
+		assertTrue(compare(value,feat3)==true);
+	}
 }
