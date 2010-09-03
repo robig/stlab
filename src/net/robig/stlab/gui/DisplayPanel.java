8c6732f6fa402d83c857c7344cd22b6e2a66756b
robig <robig@3e5e3696-bdf4-0310-96c2-9c50ceb56a63>
2010-09-03 15:08:57 +0000

    input of values

--- src/net/robig/stlab/gui/DisplayPanel.java
+++ src/net/robig/stlab/gui/DisplayPanel.java
@@ -1,21 +1,45 @@
 package net.robig.stlab.gui;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.Format;
+import java.text.NumberFormat;
+
 import static net.robig.gui.ImagePanel.loadImage;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import net.robig.gui.MyJKnob;
+import net.robig.logging.Logger;
 
 /**
  * Displays a two digit number. Is used to display the preset and Knob changes.
  * @author robig
  *
  */
-public class DisplayPanel extends JPanel {
-
+public class DisplayPanel extends JPanel implements MouseListener, PropertyChangeListener {
+	private static final long serialVersionUID = 1L;
 	Image[] digits=new Image[10];
 	Image off=null;
 	int value=0;
 	int digitWidth=0;
+	JFormattedTextField inputField=null;
+	Logger log = new Logger(getClass());
 	
 	public DisplayPanel() {
 		for(int i=0;i<10;i++){
@@ -23,9 +47,33 @@ public class DisplayPanel extends JPanel {
 		}
 		off=loadImage("img/display_off.png");
 		digitWidth=digits[0].getWidth(null);
-		setSize(digitWidth*2,2*digits[0].getHeight(null));
+		setSize(digitWidth*2,digits[0].getHeight(null));
+		
+		initComponents();
 	}
 	
+	private void initComponents() {
+		this.setLayout(null);
+		addMouseListener(this);
+		
+		Format format=NumberFormat.getNumberInstance();
+		//format.
+		inputField=new JFormattedTextField(format);
+		inputField.setOpaque(true);
+		inputField.setBounds(this.getBounds());
+		inputField.setSize(getSize());
+		inputField.setVisible(false);
+		inputField.setSelectedTextColor(Color.RED);
+		inputField.setSelectionColor(Color.DARK_GRAY);
+		inputField.setForeground(Color.RED);
+		inputField.setBackground(Color.BLACK);
+		inputField.setColumns(2);
+		Font font = new Font("Courier",inputField.getFont().getStyle(),48);
+		inputField.setFont(font);
+		inputField.addPropertyChangeListener("value",this);
+		add(inputField);
+	}
+
 	private int get1stDigit(){
 		return Math.round(value/10);
 	}
@@ -54,8 +102,8 @@ public class DisplayPanel extends JPanel {
 	}
 	
 	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
 		if(value==100){ //display 100 as 00
 			g.drawImage(digits[0],0,0,this);
@@ -65,7 +113,72 @@ public class DisplayPanel extends JPanel {
 			g.drawImage(off,0,0,this);
 		}
 		g.drawImage(digits[get2ndDigit()],digitWidth,0,this);
-		
+	}
+
+	public void onClick() {
+		inputField.setVisible(true);
+		inputField.setText(getValue()+"");
+		Runnable doRun = new Runnable() {
+			 
+            public void run() {
+            	inputField.select(0,inputField.getText().length()); 
+            }
+        };
+        SwingUtilities.invokeLater(doRun);
+
+		log.debug("Ready for input");
 	}
 	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		onClick();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+	}
+	
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("DisplayPanel Test method");
+		
+		Container thePane = myFrame.getContentPane();
+		DisplayPanel panel = new DisplayPanel();
+		// Add a DisplayPanel to the pane.
+		thePane.add(panel);
+		myFrame.setBounds(new Rectangle(0,0,100,100));
+	
+		myFrame.addWindowListener(new WindowAdapter() {
+	             public void windowClosing(WindowEvent e) {
+	                 System.exit(0);
+	             }
+	         });
+	
+		myFrame.pack();
+		myFrame.show();
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		log.debug(arg0.toString());
+		setValue(Integer.parseInt(inputField.getValue().toString()));
+		inputField.setVisible(false);
+		onChange();
+	}
+	
+	public void onChange() {
+		
+	}
 }
