96bab050374be239f509c57605b2eac804b46c3e
robig <robig@3e5e3696-bdf4-0310-96c2-9c50ceb56a63>
2011-02-18 15:26:48 +0000

    tests

--- test/net/robig/stlab/net/WebaccessTest.java
+++ test/net/robig/stlab/net/WebaccessTest.java
@@ -1,21 +1,35 @@
 package net.robig.stlab.net;
 
+import net.robig.logging.Logger;
 import net.robig.net.WebAccess;
 import net.robig.stlab.StLabConfig;
 
 import static org.testng.Assert.*;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class WebaccessTest {
 	
 	public static boolean withProxy=true;
+	public static Logger log = new Logger(WebaccessTest.class);
 
 	@BeforeSuite
 	public void prepare(){
+		StLabConfig.getInstance().setValue("environment", "DIT");
+		System.out.println("Using Environment: "+StLabConfig.getEnvironment());
+		assertFalse(StLabConfig.getEnvironmentString().equals(""),"Not testing on production environment!");
+		
 		StLabConfig.getWebProxyHost().setValue("192.168.100.2");
 		StLabConfig.getWebProxyPort().setValue(8080);
+		StLabConfig.isWebProxyEnabled().setValue(withProxy);
+	}
+	
+	@BeforeMethod
+	public void prepareTest() {
+		log.debug("prepareTest: settin Proxy: "+withProxy);
 		StLabConfig.isWebProxyEnabled().setValue(withProxy);
 	}
 	
@@ -46,5 +60,17 @@ public class WebaccessTest {
 		} finally{
 			StLabConfig.isWebProxyEnabled().setValue(withProxy);
 		}
+	}
+	
+	@Test
+	public void testRegistration(){
+		WebAccess web=new WebAccess();
+		boolean ok;
+		// Username too short
+		ok = web.register("1234", "1234", "1234");
+		assertFalse(ok);
+		// invalid email:
+		ok = web.register("unittest1234", "1234", "1234");
+		assertFalse(ok);
 	}
 }
