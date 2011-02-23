b310a5306c6df01d5ba425e7399ca5d054ea0ff9
robig <robig@3e5e3696-bdf4-0310-96c2-9c50ceb56a63>
2011-02-23 21:53:04 +0000

    get version fix

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -1,7 +1,11 @@
 #/bin/bash
-grep applicationVersion src/net/robig/stlab/StLab.java|cut -d '=' -f 2|cut -d '"' -f 2|tr "[:upper:]" "[:lower:]"|sed 's,$,-r,'|tr -d '\n'
-if [ "$1" != "" && "$1" != "production" ]; then
+from_source(){
+grep applicationVersion src/net/robig/stlab/StLab.java|cut -d '=' -f 2|cut -d '"' -f 2|tr "[:upper:]" "[:lower:]"
+}
+if [ "$1" != "" -a "$1" != "production" ]; then
+	#append revision number on non production version:
+	from_source |sed 's,$,-r,'|tr -d '\n'
 	svn info | grep Revision: | cut -d ' ' -f 2
 else
-	echo
+	from_source
 fi
